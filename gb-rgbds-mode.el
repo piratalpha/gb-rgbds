f13a4f3945e30a865410d3ff3772e66ff1cc2c4f
piratalpha <251065663+piratalpha@users.noreply.github.com>
2025-12-23 21:53:03 +0100

    Refactored and flattened

---
+++ gb-rgbds-mode.el
@@ -0,0 +1,92 @@
+;;; gb-rgbds-mode.el --- RGBDS major mode for Game Boy assembly -*- lexical-binding: t; -*-
+
+;; Author: piratalpha
+;; Keywords: languages, assembly, gameboy
+
+;; Derived from rgbds-mode by japanoise
+;; https://github.com/japanoise/rgbds-mode
+;;
+;; Original work © japanoise and contributors
+;;
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;;; Commentary:
+;; Major mode for Game Boy RGBDS assembly.
+;; Includes robust syntax highlighting and indentation rules.
+
+;;; Code:
+
+(require 'mwim)
+
+;; 1. Define Highlighting Rules
+(defconst gb-rgbds-font-lock-keywords-1
+  (list
+   ;; Comments
+   '(";.*" . font-lock-comment-face)
+   '("^\\*.*" . font-lock-comment-face)
+   ;; Instructions
+   '("\\<\\(adc\\|add\\|and\\|cp\\|dec\\|inc\\|or\\|sbc\\|sub\\|xor\\|call\\|jp\\|jr\\|ret\\|rst\\|bit\\|res\\|rl\\|rlc\\|rr\\|rrc\\|set\\|sla\\|sra\\|srl\\|swap\\|ccf\\|cpl\\|daa\\|di\\|ei\\|halt\\|nop\\|scf\\|stop\\|ld\\|pop\\|push\\|reti\\|rla\\|rlca\\|rra\\|rrca\\)\\>" . font-lock-builtin-face)
+   '("\\<\\(ADC\\|ADD\\|AND\\|CP\\|DEC\\|INC\\|OR\\|SBC\\|SUB\\|XOR\\|CALL\\|JP\\|JR\\|RET\\|RST\\|BIT\\|RES\\|RL\\|RLC\\|RR\\|RRC\\|SET\\|SLA\\|SRA\\|SRL\\|SWAP\\|CCF\\|CPL\\|DAA\\|DI\\|EI\\|HALT\\|NOP\\|SCF\\|STOP\\|LD\\|POP\\|PUSH\\|RETI\\|RLA\\|RLCA\\|RRA\\|RRCA\\)\\>" . font-lock-builtin-face)
+   ;; Registers
+   '("\\<\\(a\\|b\\|c\\|d\\|e\\|h\\|l\\|af\\|bc\\|de\\|hl\\|sp\\|A\\|B\\|C\\|D\\|E\\|H\\|L\\|AF\\|BC\\|DE\\|HL\\|SP\\)\\>" . font-lock-variable-name-face)
+   ;; Labels
+   '("\\(\\w*:\\)"  . font-lock-function-name-face))
+  "Minimal highlighting expressions for rgbds mode.")
+
+(defconst gb-rgbds-font-lock-keywords-2
+  (append gb-rgbds-font-lock-keywords-1
+          (list
+           ;; Constants / Numbers
+           '("\\<\\(\\([0-9][0-9A-Fa-f]*[Hh]\\|\\(0[Xx]\\|[0-9]\\|\\$[0-9A-Fa-f]\\)[0-9A-Fa-f]*\\)\\|[01][01]*[Bb]\\|%[01][01]*\\|[0-9]*\\)\\>" . font-lock-constant-face)
+           ;; Immediate value symbol ($)
+           '("\\(\\$\\)" . font-lock-warning-face)))
+  "Additional Keywords to highlight in rgbds mode.")
+
+(defconst gb-rgbds-font-lock-keywords-3
+  (append gb-rgbds-font-lock-keywords-2
+          (list
+           ;; Local labels / Macro args
+           '("\\(\\.\\w*\\|#\\w*\\)" . font-lock-preprocessor-face)
+           ;; Preprocessor Directives
+           '("\\<\\(DB\\|DW\\|DS\\|SECTION\\|EQU\\|EQUS\\|SET\\|POPS\\|PUSHS\\|MACRO\\|ENDM\\|RSSET\\|RSRESET\\|RB\\|RW\\|SHIFT\\|EXPORT\\|GLOBAL\\|PURGE\\|INCBIN\\|UNION\\|NEXTU\\|ENDU\\|PRINTT\\|PRINTI\\|PRINTV\\|PRINTF\\|REPT\\|ENDR\\|FAIL\\|WARN\\|INCLUDE\\|IF\\|ELIF\\|ELSE\\|ENDC\\|CHARMAP\\)\\>" . font-lock-keyword-face)
+           '("\\<\\(db\\|dw\\|ds\\|section\\|equ\\|equs\\|set\\|pops\\|pushs\\|macro\\|endm\\|rsset\\|rsreset\\|rb\\|rw\\|shift\\|export\\|global\\|purge\\|incbin\\|union\\|nextu\\|endu\\|printt\\|printi\\|printv\\|printf\\|rept\\|endr\\|fail\\|warn\\|include\\|if\\|elif\\|else\\|endc\\|charmap\\)\\>" . font-lock-keyword-face)))
+  "Full highlighting for rgbds mode.")
+
+(defvar gb-rgbds-font-lock-keywords gb-rgbds-font-lock-keywords-3
+  "Default highlighting expressions for rgbds mode.")
+
+;; 2. Syntax Table (Defines what is a 'word' vs 'punctuation')
+(defvar gb-rgbds-mode-syntax-table
+  (let ((st (make-syntax-table)))
+    (modify-syntax-entry ?_ "w" st)   ; Underscore is part of a word
+    (modify-syntax-entry ?# "w" st)   ; Hash is part of a word
+    (modify-syntax-entry ?. "w" st)   ; Dot is part of a word
+    (modify-syntax-entry ?\; "<" st)  ; Semi-colon starts comment
+    (modify-syntax-entry ?\n ">" st)  ; Newline ends comment
+    (modify-syntax-entry ?\t "-" st)  ; Tab is whitespace
+    st)
+  "Syntax table for gb-rgbds-mode.")
+
+;; 3. The Mode Definition
+;;;###autoload
+(define-derived-mode gb-rgbds-mode prog-mode "GB-RGBDS"
+  "Major mode for Game Boy RGBDS assembly."
+  (setq-local font-lock-defaults '(gb-rgbds-font-lock-keywords))
+  (setq-local comment-start ";")
+  (setq-local comment-end "")
+  (setq-local indent-tabs-mode nil)) 
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.asm\\'" . gb-rgbds-mode))
+
+;; 4. Keybindings
+(define-key gb-rgbds-mode-map (kbd "C-j") #'newline-and-indent)
+(define-key gb-rgbds-mode-map (kbd "RET") #'newline-and-indent)
+(define-key gb-rgbds-mode-map (kbd "C-a") #'mwim-beginning-of-code-or-line)
+(define-key gb-rgbds-mode-map (kbd "C-e") #'mwim-end-of-code-or-line)
+
+(provide 'gb-rgbds-mode)
+;;; gb-rgbds-mode.el ends here
--- gb-rgbds.el
+++ gb-rgbds.el
@@ -1,26 +1,19 @@
 ;;; gb-rgbds.el --- Game Boy RGBDS helpers -*- lexical-binding: t; -*-
 
 ;; Author: piratalpha
-;; Version: 0.1.0
+;; Version: 0.2.0
 ;; Package-Requires: ((emacs "27.1") (mwim "0.9"))
 ;; Keywords: tools, gameboy, rgbds
 ;; URL: https://github.com/piratalpha/gb-rgbds
 
 ;;; Commentary:
 ;;
-;; Simple Game Boy RGBDS build + run helpers.
+;; Game Boy RGBDS workflow helpers.
+;; Includes commands to build, run, and debug ROMs.
 ;;
-;; Provides commands:
-;; - gb-rgbds-build
-;; - gb-rgbds-run
-;; - gb-rgbds-build-and-run
-;;
-
-(add-to-list 'load-path
-             (expand-file-name "lisp"
-                               (file-name-directory load-file-name)))
 
 (require 'gb-rgbds-mode)
+(require 'compile)
 
 ;;; Code:
 
@@ -28,62 +21,86 @@
   "Game Boy RGBDS workflow helpers."
   :group 'tools)
 
-(defcustom gb-rgbds-main-file "main.asm"
-  "Main ASM file for the Game Boy project."
-  :type 'string)
+;; -- Configuration Variables --
 
-(defcustom gb-rgbds-emulicious-jar
-  "~/tools/emulicious/emulicious.jar"
-  "Path to Emulicious.jar."
+(defcustom gb-rgbds-main-asm-file "main.asm"
+  "The entry point assembly file for the project."
+  :type 'string
+  :group 'gb-rgbds)
+
+(defcustom gb-rgbds-output-rom-name "main.gb"
+  "The name of the resulting ROM file."
+  :type 'string
+  :group 'gb-rgbds)
+
+(defcustom gb-rgbds-build-command
+  "rgbasm -o main.o %s && rgblink -o %s main.o && rgbfix -v -p 0 %s"
+  "Command string to build the project. 
+%s placeholders are: source file, output rom, output rom."
+  :type 'string
+  :group 'gb-rgbds)
+
+(defcustom gb-rgbds-emulicious-jar nil
+  "Path to the Emulicious.jar file."
   :type 'file
   :group 'gb-rgbds)
 
-;;;###autoload
+;; -- Internal Helpers --
+
 (defun gb-rgbds--project-root ()
-  "Return the project root containing `gb-rgbds-main-file`."
-  (or (locate-dominating-file default-directory gb-rgbds-main-file)
-      (error "No %s found in any parent directory" gb-rgbds-main-file)))
+  "Find the directory containing the main asm file."
+  (or (locate-dominating-file default-directory gb-rgbds-main-asm-file)
+      (error "Could not find %s in directory hierarchy." gb-rgbds-main-asm-file)))
+
+;; -- Public Commands --
 
 ;;;###autoload
 (defun gb-rgbds-build ()
-  "Build the Game Boy ROM using RGBDS."
+  "Build the Game Boy ROM using the defined build command."
   (interactive)
   (let* ((root (gb-rgbds--project-root))
-         (default-directory root)
-         (cmd
-          (format
-           "rgbasm -o main.o %s && rgblink -o main.gb main.o && rgbfix -v -p 0 main.gb"
-           gb-rgbds-main-file))
-         (compilation-buffer-name-function
-          (lambda (_) "*gb-rgbds-build*")))
-    (compile cmd)))
+         (default-directory root)) ; Execute command in project root
+    (compile (format gb-rgbds-build-command 
+                     gb-rgbds-main-asm-file
+                     gb-rgbds-output-rom-name
+                     gb-rgbds-output-rom-name))))
 
 ;;;###autoload
 (defun gb-rgbds-run ()
   "Run the built ROM in Emulicious."
   (interactive)
-  (unless gb-rgbds-emulicious-jar
-    (error "gb-rgbds-emulicious-jar is not set"))
   (let* ((root (gb-rgbds--project-root))
-         (rom (expand-file-name "main.gb" root)))
-    (unless (file-exists-p rom)
-      (error "ROM not found, build first"))
-    (start-process
-     "emulicious"
-     "*gb-rgbds-emulicious*"
-     "java" "-jar" gb-rgbds-emulicious-jar rom)))
+         (rom-path (expand-file-name gb-rgbds-output-rom-name root)))
+    
+    ;; Sanity Checks
+    (unless gb-rgbds-emulicious-jar
+      (user-error "Variable `gb-rgbds-emulicious-jar' is not set. Check your config."))
+    (unless (file-exists-p gb-rgbds-emulicious-jar)
+      (user-error "Emulicious jar not found at: %s" gb-rgbds-emulicious-jar))
+    (unless (file-exists-p rom-path)
+      (user-error "ROM file not found: %s. Build it first!" rom-path))
+
+    (message "Starting Emulicious...")
+    (start-process "emulicious" 
+                   "*gb-rgbds-emulicious*" 
+                   "java" "-jar" gb-rgbds-emulicious-jar rom-path)))
 
 ;;;###autoload
 (defun gb-rgbds-build-and-run ()
-  "Build and run the ROM."
+  "Build the project, and if successful, run the emulator."
   (interactive)
-  (let ((hook nil))
-    (setq hook
-          (lambda (_buf status)
-            (when (string-match-p "finished" status)
-              (gb-rgbds-run))
-            (remove-hook 'compilation-finish-functions hook)))
-    (add-hook 'compilation-finish-functions hook)
+  ;; We hook into the compilation finish process
+  (let ((finish-hook nil))
+    (setq finish-hook
+          (lambda (buffer status)
+            ;; Check if compilation finished successfully (no "exited abnormally")
+            (if (string-match-p "^finished" status)
+                (gb-rgbds-run)
+              (message "Build failed, not running emulator."))
+            ;; Cleanup the hook so it doesn't run on future compilations
+            (remove-hook 'compilation-finish-functions finish-hook)))
+    
+    (add-hook 'compilation-finish-functions finish-hook)
     (gb-rgbds-build)))
 
 (provide 'gb-rgbds)
